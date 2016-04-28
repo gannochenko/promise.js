2f47f8c700ce55c1f8b1ade8c23065d33127a8e6
Sergey Gannochenko <Sergey Gannochenko>
2016-04-28 03:15:37 +0200

    the first commit, and the last i suppose

---
+++ README.md
@@ -0,0 +1,10 @@
+Promise.js: an implementation of Promises/A+
+============================================
+
+This is an implementation of "promise" code pattern for Javascript, that is compatible with [Promises/A+](https://promisesaplus.com/) standard.
+The code written "just for self education purposes", no new ideas introduced.
+
+Examples & tests
+----------------
+
+See tests/index.html for examples and details.
---
+++ src/promise.js
@@ -0,0 +1,196 @@
+(function(){
+
+	Promise = function()
+	{
+		this.state = null;
+		this.value = null;
+		this.reason = null;
+		this.next = null;
+
+		this.onFulfilled = [];
+		this.onRejected = [];
+	};
+	Promise.prototype.fulfill = function(value)
+	{
+		this.checkState();
+
+		this.value = value;
+		this.state = true;
+		this.execute();
+	};
+	Promise.prototype.reject = function(reason)
+	{
+		this.checkState();
+
+		this.reason = reason;
+		this.state = false;
+		this.execute();
+	};
+	Promise.prototype.then = function(onFulfilled, onRejected)
+	{
+		if(Util.isFunction(onFulfilled))
+		{
+			this.onFulfilled.push(onFulfilled);
+		}
+		if(Util.isFunction(onRejected))
+		{
+			this.onRejected.push(onRejected);
+		}
+
+		return this.execute();
+	};
+	/**
+	 * Resolve function. This allows to put *this* promise in dependence of
+	 * some other value (which could be an instance of the promise too)
+	 * @param x
+	 */
+	Promise.prototype.resolve = function(x)
+	{
+		var this_ = this;
+
+		if(this === x)
+		{
+			this.reject(new TypeError('Promise cannot fulfill or reject itself')); // avoid recursion
+		}
+		else if(x instanceof Promise)
+		{
+			x.then(function(value){
+				this_.fulfill(value);
+			}, function(reason){
+				this_.reject(reason);
+			});
+		}
+		else if(Util.isFunction(x) || Util.isObject(x)) // process i.e. "thenable" - a poorly-written promises
+		{
+			var then = x.then;
+			if(Util.isFunction(then))
+			{
+				var executed = false;
+
+				then.apply(x, [
+					function resolvePromise(y){
+						if(!executed)
+						{
+							executed = true;
+							this_.resolve(y);
+						}
+					},
+					function rejectPromise(r){
+						if(!executed)
+						{
+							executed = true;
+							this_.reject(r);
+						}
+					}
+				]);
+			}
+			else // Object is not thenable
+			{
+				this.fulfill(x);
+			}
+		}
+		else
+		{
+			this.fulfill(x);
+		}
+	};
+	Promise.prototype.execute = function()
+	{
+		if(this.next === null)
+		{
+			this.next = new Promise();
+		}
+
+		if(this.state === null)
+		{
+			//then() must not be called before promise resolve() happens
+			return this.next;
+		}
+
+		var value = undefined;
+		var reason = undefined;
+		var x = undefined;
+		var k;
+		if(this.state === true) // promise was fulfilled
+		{
+			if(this.onFulfilled.length)
+			{
+				try
+				{
+					for(k = 0; k < this.onFulfilled.length; k++)
+					{
+						x = this.onFulfilled[k].apply(this, [this.value]);
+						if(typeof x != 'undefined')
+						{
+							value = x;
+						}
+					}
+				}
+				catch(e)
+				{
+					reason = e; // reject next
+				}
+			}
+			else
+			{
+				value = this.value; // resolve next
+			}
+		}
+		else if(this.state === false) // promise was rejected
+		{
+			if(this.onRejected.length)
+			{
+				try
+				{
+					for(k = 0; k < this.onRejected.length; k++)
+					{
+						x = this.onRejected[k].apply(this, [this.reason]);
+						if(typeof x != 'undefined')
+						{
+							value = x;
+						}
+					}
+				}
+				catch(e)
+				{
+					reason = e; // reject next
+				}
+			}
+			else
+			{
+				reason = this.reason; // reject next
+			}
+		}
+
+		if(typeof reason != 'undefined')
+		{
+			this.next.reject(reason);
+		}
+		else if(typeof value != 'undefined')
+		{
+			// run this.next resolve, which eventually calls this.next.fulfill() or this.next.reject()
+			this.next.resolve(value);
+		}
+
+		return this.next;
+	};
+	Promise.prototype.checkState = function()
+	{
+		if(this.state !== null)
+		{
+			throw new Error('You can not do fulfill() or reject() multiple times');
+		}
+	};
+
+	var Util = {
+		isFunction: function(arg)
+		{
+			return Object.prototype.toString.call(arg) == '[object Function]';
+		},
+		isObject: function(arg)
+		{
+			return Object.prototype.toString.call(arg) == '[object Object]';
+		}
+	};
+
+}).call(this);
